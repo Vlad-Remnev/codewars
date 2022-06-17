873bbd1b2437f64edc001d70b64bc094d07bdbaf
Vladislav Remnev <remnevvlad30@gmail.com>
2022-06-17 15:59:25 +0300

    Update script.js
    banjo func

--- script.js
+++ script.js
@@ -11,3 +11,20 @@ function filter_list(l) {
 }
 
 console.log(filter_list([22, 33, 'dsfdsf', 'asd', '123', -1]))
+
+// banjo
+
+function areYouPlayingBanjo(name) {
+    const newName = name.toLowerCase().split('');
+    if (newName[0] == 'r') {
+        return name + " plays banjo"
+    } else {
+        return name + " does not play banjo"
+    }
+}
+
+console.log(areYouPlayingBanjo('Adam'))
+console.log(areYouPlayingBanjo('Ringo'))
+
+
+
