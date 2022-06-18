06d23027390761b2c0656879a3402fe32a93075c
Vladislav Remnev <remnevvlad30@gmail.com>
2022-06-18 11:29:00 +0300

    Update script.js
    Reqursion add

--- script.js
+++ script.js
@@ -134,3 +134,14 @@ function cc(card) {
         return count + holdBet
     }
 }
+
+//recursion
+
+function sum(arr, n) {
+    if (n <= 0) {
+      return 0
+    } else {
+      return sum(arr, n - 1) + arr[n - 1]
+    }
+  }
+console.log(sum([2, 3, 4], 1))
