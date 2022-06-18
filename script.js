3501253e693434af57ff7b925e878481fb23af82
Vladislav Remnev <remnevvlad30@gmail.com>
2022-06-18 15:27:53 +0300

    Update script.js
    delete spaces func

--- script.js
+++ script.js
@@ -221,3 +221,11 @@ function getMiddle(s) {
 
 console.log(getMiddle("testing"))
 console.log(getMiddle("test"))
+
+//delete spaces
+
+function noSpace(x){
+    return x.replace(/\s+/g, '');
+}
+
+console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'))
